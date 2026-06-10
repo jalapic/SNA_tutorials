1ed16757d29e2fb07e2da417411daa8a5fa1d57f
amutabanna <aziz@mutabanna.com>
2026-06-10 10:45:27 -0500

    found isomorph lookup function

--- Summer2026/code/tasks/isomorph_lookup.R
+++ Summer2026/code/tasks/isomorph_lookup.R
@@ -1,56 +1,12 @@
 # ------------------ isomorph lookup + transitions ------------------
 
-build_canon_index <- function(reps) {
-  keys <- vapply(reps, function(G) {
-    p  <- igraph::canonical_permutation(G)$labeling
-    gc <- igraph::permute(G, p)
-    M  <- igraph::as_adjacency_matrix(gc, sparse = FALSE)
-    paste(as.integer(M), collapse = "")
-  }, "")
-  structure(seq_along(reps), names = keys)
-}
+# get graph_list (isomorph dictionary)
+d <- readRDS("rfid_data/nodes6_g.RDS")
 
-isomorph_class_id <- function(g, index_map) {
-  p  <- igraph::canonical_permutation(g)$labeling
-  gc <- igraph::permute(g, p)
-  M  <- igraph::as_adjacency_matrix(gc, sparse = FALSE)
-  key <- paste(as.integer(M), collapse = "")
-  unname(index_map[[key]])
-}
+# get g <- graph_from_edgelist(last_ints)
 
-generate_tournament_graph <- function(n = 6) {
-  edges <- integer(0)
-  for (i in 1:(n - 1L)) for (j in (i + 1L):n) {
-    if (runif(1) < 0.5) edges <- c(edges, i, j) else edges <- c(edges, j, i)
-  }
-  igraph::graph(edges, directed = TRUE)
-}
-
-flip_edge <- function(g) {
-  E <- igraph::as_edgelist(g, names = FALSE)
-  flip_index <- sample(nrow(E), 1L)
-  e <- E[flip_index, ]
-  Enew <- rbind(E[-flip_index, , drop = FALSE], c(e[2], e[1]))
-  igraph::graph_from_edgelist(Enew, directed = TRUE)
-}
-
-simulate_tournament_transitions <- function(N = 1000, reps, index_map) {
-  n <- igraph::vcount(reps[[1]])
-  g <- generate_tournament_graph(n)
-  path <- integer(N + 1L)
-  path[1] <- isomorph_class_id(g, index_map)
-  for (i in 1:N) {
-    g <- flip_edge(g)
-    path[i + 1L] <- isomorph_class_id(g, index_map)
-  }
-  path
-}
-
-create_transition_matrix <- function(sequence, num_states) {
-  TM <- matrix(0L, nrow = num_states, ncol = num_states)
-  for (i in 1:(length(sequence) - 1L)) {
-    from <- sequence[i]; to <- sequence[i + 1L]
-    if (!is.na(from) && !is.na(to)) TM[from, to] <- TM[from, to] + 1L
-  }
-  TM
+# Return the index or NA if no match found
+check_isomorphism <- function(g, graph_list) {
+  match <- which(unlist(lapply(graph_list, function(x) graph.isomorphic(g, x))))
+  return(ifelse(length(match) > 0, match, NA))
 }
