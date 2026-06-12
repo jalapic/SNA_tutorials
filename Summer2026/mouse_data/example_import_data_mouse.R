5ed80bf6b0bb9872a6fb5199c4d86535a67f955d
amutabanna <aziz@mutabanna.com>
2026-06-12 15:14:00 -0500

    cleans up code to turn cohort data into isomorph class data

--- Summer2026/mouse_data/example_import_data_mouse.R
+++ Summer2026/mouse_data/example_import_data_mouse.R
@@ -3,43 +3,36 @@
 library(tidyverse)
 library(igraph)
 
-c7 <- read_csv("Summer2026/mouse_data/cohort7.csv")
+cohort <- read_csv("Summer2026/mouse_data/cohort7.csv")
 
-head(c7)
+head(cohort)
 
 #make sure data is ordered by time (value 1 column)
-c7 <- c7 %>% arrange(value1)
+cohort <- cohort %>% arrange(value1)
 
 # Data are now in time ordered sequence.
-head(c7)
-
+head(cohort)
 
 # the winner loser columns are #vector1 (winner) and #vector2 (loser)
-
-table(c7$vector1)
-table(c7$vector2)
+table(cohort$vector1)
+table(cohort$vector2)
 
 # install if needed
 # devtools::install_github('jalapic/compete')
 
-
-
 # this just shows that there is a hierarchy
-mat <- compete::get_wl_matrix(as.matrix(c7[,4:5]))
+mat <- compete::get_wl_matrix(as.matrix(cohort[,4:5]))
 mat
 isi.res <- compete::isi13(mat)
 isi.res
 compete::get_di_matrix(mat)[isi.res$best_order,isi.res$best_order]
 
-
-head(c7)
-
+head(cohort)
 
 
 ### Get last interactions
 
 # devtools::install_github("jalapic/hierformR") 
-
 
 library(hierformR)
 
@@ -51,25 +44,23 @@ cohort_lastints <- function(cohort = NULL) {
 
 
 # makes all cohorts just the last interactions
+edgelists <- cohort_lastints(cohort)   # this is by row, not whole dataset
 
+edgelists <- tibble(edgelists)
 
-results <- cohort_lastints(c7)   # this should be for the whole dataset
+nrow(edgelists)
 
-results <- tibble(results)
-
-nrow(results)
-
-# cohort_lastints(c7[1:25,]) # creates last int. dataset for rows 1-25
-results$results[25]
+# cohort_lastints(cohort[1:25,]) # creates last int. dataset for rows 1-25
+edgelists$edgelists[25]
 
 # for example....
 
-# Loop through each nested tibble and convert it to a graph
-graphs <- lapply(results$results, function(df) {
+# loops through each nested tibble and converts it to a graph
+graphs <- lapply(edgelists$edgelists, function(df) {
   graph_from_data_frame(df, directed = TRUE)
 })
 
-# Access your separate graphs by index
+# accesses separate graphs by index
 graphs[[25]] # Graph from row 25
 
 graphs <- tibble(graphs)
