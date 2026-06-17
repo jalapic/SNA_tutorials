bb1cd88ec89bdf059ffd66ae25052d2bfe805bf5
Your Name <you@example.com>
2026-06-17 17:57:12 -0500

    working on downloading files

--- Summer2026/mousetropolis/consistent_timing_check.R
+++ Summer2026/mousetropolis/consistent_timing_check.R
@@ -11,3 +11,9 @@
 # 6. look for our gaps (missing mice, jumping mice, missing antennas)
 # 7. identify transitions for each cage for each mouse
 # 8. determine which mice are following each other (within 250 miliseconds of each other)
+
+# import library
+library(here)
+
+# import data
+april18 <- read.csv("data/rawdata20260418.csv")
