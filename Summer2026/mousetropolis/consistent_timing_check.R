c2934aa18a6a049d6d68dcc4e34bf9f34953b26d
Your Name <you@example.com>
2026-06-19 11:16:57 -0500

    timing checks

--- Summer2026/mousetropolis/consistent_timing_check.R
+++ Summer2026/mousetropolis/consistent_timing_check.R
@@ -12,8 +12,25 @@
 # 7. identify transitions for each cage for each mouse
 # 8. determine which mice are following each other (within 250 miliseconds of each other)
 
-# import library
-library(here)
+# import libraries
+
 
 # import data
-april18 <- read.csv("data/rawdata20260418.csv")
+april18 <- read.csv("Summer2026/mousetropolis/data/rawdata20260418.csv", sep = ";")
+april19 <- read.csv("Summer2026/mousetropolis/data/rawdata20260419.csv", sep = ";")
+april20 <- read.csv("Summer2026/mousetropolis/data/rawdata20260420.csv", sep = ";")
+april21 <- read.csv("Summer2026/mousetropolis/data/rawdata20260421.csv", sep = ";")
+april22 <- read.csv("Summer2026/mousetropolis/data/rawdata20260422.csv", sep = ";")
+april23 <- read.csv("Summer2026/mousetropolis/data/rawdata20260423.csv", sep = ";")
+april24 <- read.csv("Summer2026/mousetropolis/data/rawdata20260424.csv", sep = ";")
+april25 <- read.csv("Summer2026/mousetropolis/data/rawdata20260425.csv", sep = ";")
+april26 <- read.csv("Summer2026/mousetropolis/data/rawdata20260426.csv", sep = ";")
+april27 <- read.csv("Summer2026/mousetropolis/data/rawdata20260427.csv", sep = ";")
+april28 <- read.csv("Summer2026/mousetropolis/data/rawdata20260428.csv", sep = ";")
+april29 <- read.csv("Summer2026/mousetropolis/data/rawdata20260429.csv", sep = ";")
+april30 <- read.csv("Summer2026/mousetropolis/data/rawdata20260430.csv", sep = ";")
+may1 <- read.csv("Summer2026/mousetropolis/data/rawdata20260501.csv", sep = ";")
+may2 <- read.csv("Summer2026/mousetropolis/data/rawdata20260502.csv", sep = ";")
+
+# make sure clock doesn't start each day
+
