026b3c4732c42df5fd74eb2dfdd6491144cac55c
amutabanna <aziz@mutabanna.com>
2026-06-24 14:46:27 -0500

    halfway redid the script to account for changes to transition identifier - new file created for all true transitions but not for unique following pairs

--- Summer2026/mousetropolis/mice_following_others.R
+++ Summer2026/mousetropolis/mice_following_others.R
@@ -27,41 +27,41 @@ all_files <- c(
   "Summer2026/mousetropolis/data/rawdata20260502.csv"
 )
 
-all_transitions <- lapply(all_files, process_mouse_csv) %>% bind_rows()
-all_true <- all_transitions %>% filter(true_transition == TRUE)
+all_true_transitions <- lapply(all_files, check_mouse_transitions) %>% bind_rows()
+
 
 # could be useful???
-saveRDS(all_true, "Summer2026/mousetropolis/data/all_true.RDS")
-#all_true <- readRDS("Summer2026/mousetropolis/data/all_true.RDS")
+saveRDS(all_true_transitions, "Summer2026/mousetropolis/data/all_true_trans.RDS")
+att <- readRDS("Summer2026/mousetropolis/data/all_true_trans.RDS")
 
 # mice that follow within 250 milliseconds
 find_following <- function(df, window_ms = 250) {
-  true_trans <- df[df$true_transition == TRUE & !is.na(df$true_transition), ]
+  true_trans <- df
   
   results <- list()
   
   for (i in 1:nrow(true_trans)) {
     mid   <- true_trans$mouse_id[i]
-    bfrom <- true_trans$box_from[i]
-    bto   <- true_trans$box_to[i]
-    ts    <- true_trans$cantimestamp[i]
+    bfrom <- true_trans$from_box[i]
+    bto   <- true_trans$to_box[i]
+    ts    <- true_trans$start_cantimestamp[i]
     
     matches <- true_trans[
       true_trans$mouse_id != mid &
-        true_trans$box_from == bfrom &
-        true_trans$box_to == bto &
-        abs(true_trans$cantimestamp - ts) <= window_ms, ]
+        true_trans$from_box == bfrom &
+        true_trans$to_box == bto &
+        abs(true_trans$start_cantimestamp - ts) <= window_ms, ]
     
     if (nrow(matches) > 0) {
       for (j in 1:nrow(matches)) {
         results[[length(results) + 1]] <- data.frame(
           mouse_a = mid,
           mouse_b = matches$mouse_id[j],
-          box_from = bfrom,
-          box_to = bto,
+          from_box = bfrom,
+          to_box = bto,
           time_a = ts,
-          time_b = matches$cantimestamp[j],
-          time_diff_ms = abs(ts - matches$cantimestamp[j])
+          time_b = matches$start_cantimestamp[j],
+          time_diff_ms = abs(ts - matches$start_cantimestamp[j])
         )
       }
     }
@@ -71,13 +71,13 @@ find_following <- function(df, window_ms = 250) {
 }
 
 # find all following pairs
-following_pairs <- find_following(all_true)
+following_pairs <- find_following(att)
 
 following_pairs_unique <- following_pairs %>%
   rowwise() %>%
   mutate(pair = paste(sort(c(mouse_a, mouse_b)), collapse = "_")) %>%
   ungroup() %>%
-  distinct(pair, box_from, box_to, .keep_all = TRUE) %>%
+  distinct(pair, from_box, to_box, .keep_all = TRUE) %>%
   select(-pair)
 
 # save unique pairs as document
