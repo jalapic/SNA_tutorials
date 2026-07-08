79c53e0a9e4cca4a4185f14954dbf1ba83eb478e
amutabanna <aziz@mutabanna.com>
2026-07-08 12:55:28 -0500

    added transitive/intransitive triad counts to tables and color-coded median interactions bar graph by intransitive triad count

--- Summer2026/mouse_data/timeline_code/interactions_per_state.R
+++ Summer2026/mouse_data/timeline_code/interactions_per_state.R
@@ -17,7 +17,15 @@ wide_state_counts <- state_counts %>%
   ) %>%
   arrange(isomorph_class)
 
-wide_state_counts
+# add columns with transitive and intransitive triad counts
+# from Summer2026/code/tasks/tables/isomorph-tables_3-6nodes.R
+full_state_counts <- wide_state_counts %>%
+  left_join(
+    df6 %>% select(Tournament, Transitive_Triads, Intransitive_Triads),
+    by = c("isomorph_class" = "Tournament")
+  )
+
+View(full_state_counts)
 
 # find median number of within-state interactions across all cohorts
 median_ints <- state_counts %>%
@@ -27,12 +35,22 @@ median_ints <- state_counts %>%
 
 median_ints
 
-median_dist <- ggplot(data = median_ints, 
-                      aes(x = factor(isomorph_class), y = median_interactions)) +
+full_median_ints <- median_ints %>%
+  left_join(
+    df6 %>% select(Tournament, Transitive_Triads, Intransitive_Triads),
+    by = c("isomorph_class" = "Tournament")
+  )
+
+full_median_ints
+
+median_dist <- ggplot(data = full_median_ints, 
+                      aes(x = factor(isomorph_class), y = median_interactions,
+                          fill = factor(Intransitive_Triads))) +
   geom_col() +
   ggtitle("Median Number of Interactions Per Isomorph Class") +
   xlab("Isomorph Class") +
   ylab("Median Number of Interactions") +
+  labs(fill = "Intransitive Triad Count") +
   theme_bw() +
   scale_x_discrete(minor_breaks = seq(1, 56, by = 1)) +
   scale_y_continuous(expand = expansion(mult = c(0, 0.05)))
